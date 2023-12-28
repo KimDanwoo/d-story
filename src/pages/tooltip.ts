b59acc0875c122f678dec33f33c2ba56e9bc6c26
KimDanwoo <dudgh9198@naver.com>
2023-12-28 14:49:39 +0900

    Revert "🎨 코드 수정"
    This reverts commit f576deea7b5bad4cc5edc5743f26b48abd41c920.

--- src/pages/tooltip.ts
+++ src/pages/tooltip.ts
@@ -1,8 +1,9 @@
 import { html, css, LitElement } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 
-const enterEvents = ['click']
-const leaveEvents = ['blur', 'keydown']
+// Events to turn on/off the tooltip
+const enterEvents = ['pointerenter', 'focus']
+const leaveEvents = ['pointerleave', 'blur', 'keydown', 'click']
 
 @customElement('simple-tooltip')
 export class SimpleTooltip extends LitElement {
@@ -26,6 +27,8 @@ export class SimpleTooltip extends LitElement {
       transform: scale(1);
     }
   `
+
+  updateExternalElement() {}
 
   static lazy(target: Element, callback: (target: SimpleTooltip) => void) {
     const createTooltip = () => {
@@ -52,6 +55,7 @@ export class SimpleTooltip extends LitElement {
   get target() {
     return this._target
   }
+
   set target(target: Element | null) {
     if (this.target) {
       enterEvents.forEach((name) =>
@@ -66,11 +70,6 @@ export class SimpleTooltip extends LitElement {
       enterEvents.forEach((name) => target!.addEventListener(name, this.show))
       leaveEvents.forEach((name) => target!.addEventListener(name, this.hide))
     }
-    if (!this.target && !target) {
-      window.addEventListener('click', this.hide)
-    }
-    console.log(target)
-    console.log(this.target)
     this._target = target
   }
 
@@ -105,5 +104,14 @@ export class SimpleTooltip extends LitElement {
     if (!this.showing) {
       this.style.display = 'none'
     }
+  }
+
+  addGlobalClickListener() {
+    document.addEventListener('click', (event: Element) => {
+      // 클릭된 요소가 이 엘리먼트 내부에 있는지 확인
+      if (!this.contains(event.target)) {
+        this.hide()
+      }
+    })
   }
 }
