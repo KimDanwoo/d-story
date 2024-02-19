6f60f8842ce6079e5bc4856dddff7a210bd2727b
KimDanwoo <dudgh9198@naver.com>
2024-02-19 16:30:16 +0900

    🔧 action setting -1

--- src/components/Col/Col.stories.ts
+++ src/components/Col/Col.stories.ts
@@ -7,10 +7,7 @@ const meta = {
   title: 'Components/Col',
   tags: ['autodocs'],
   render: (args: ColProps) =>
-    html`<base-col
-      items=${args.items || 'start'}
-      content=${args.content || 'between'}
-    >
+    html`<base-col items=${args.items || 'start'} content=${args.content || 'between'}>
       <p>1번 항목</p>
       <p>2번 항목</p>
       <p>3번 항목</p>
@@ -33,7 +30,7 @@ type Story = StoryObj<ColProps>
 export const Default: Story = {
   args: {
     items: 'center',
-    content: 'between',
+    content: 'around',
   },
 }
 export const Primary: Story = {
